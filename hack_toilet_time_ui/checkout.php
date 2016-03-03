3c27a9237e632c7eb40935c6867e7692b297e2ae
Ayoub Sbai <sbai.ayoub@hotmail.com>
2016-03-03 23:25:26 +0100

    Update checkout.php

--- hack_toilet_time_ui/checkout.php
+++ hack_toilet_time_ui/checkout.php
@@ -5,9 +5,9 @@ error_reporting(-1);
 require_once 'braintree/lib/Braintree.php';
 
 Braintree_Configuration::environment('sandbox');
-Braintree_Configuration::merchantId('qcdrb979q7b932g5');
+Braintree_Configuration::merchantId('qcdrb97rq7b932g5');
 Braintree_Configuration::publicKey('8tcf88kn9gp3rbgt');
-Braintree_Configuration::privateKey('b3e602fa0b47b2ba3ef449f58013963e');
+Braintree_Configuration::privateKey('b3e602fa0b47b2ba3ef4r9f58013963e');
 
 if(isset($_GET['tocken'])){
 	echo($clientToken = Braintree_ClientToken::generate());
